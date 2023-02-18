43b89f711c2173da1dc88a5d438e7958e0482dc0
Sheroz-Pulatov <123168056+Sheroz-Pulatov@users.noreply.github.com>
2023-02-18 15:06:35 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,138 @@
+
+let arr = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+]
+
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {
+        course: '   BACKEND',
+        count: 0
+    },
+]
+
+
+let words;
+for (let folder of categories) {
+    folder.course = folder.course.trim();
+    if (folder.course !== 'Dizayn') {
+        folder.course = folder.course.toLowerCase();
+    } else if (folder.course === 'SMM' || folder.course === '3dsmax')
+        folder.course = folder.course[0].toUpperCase() + folder.course.slice(1);
+};
+
+for (let folder of arr) {
+    words = folder.info.faculity;
+    for (let folder of categories) {
+        if (folder.course === words) {
+            folder.count++;
+        };
+    };
+};
+
+
+console.log(
+    categories
+);
